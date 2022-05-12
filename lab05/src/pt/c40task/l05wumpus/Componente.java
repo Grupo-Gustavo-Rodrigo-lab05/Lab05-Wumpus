c0ec2976ea8577b2b48455f601e63f4cc99e26ed
Rodrigo Botelho Zuiani <ra245244@zecolmeia.lab.ic.unicamp.br>
2022-05-12 15:56:46 -0300

    Cria classe Componente

---
+++ lab05/src/pt/c40task/l05wumpus/Componente.java
@@ -0,0 +1,9 @@
+package pt.c40task.l05wumpus;
+
+public abstract class Componente {
+    protected int coordenadaX, coordenadaY;
+    protected Caverna caverna;
+
+    public abstract void setCoordenadas(int coordenadaX, int coordenadaY);
+    public abstract void conectaCaverna(Caverna caverna);
+}
